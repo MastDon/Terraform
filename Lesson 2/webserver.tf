9c17e52be7de081b900d880ba414f87f7974742c
MastDon <sid2@yandex.ru>
2019-12-03 17:44:18 +0200

    Create .tf file for webserver

--- Lesson 2/webserver.tf
+++ Lesson 2/webserver.tf
@@ -1,3 +1,17 @@
 provider "aws" {
     region = "eu-central-1"
 }
+
+
+resource "aws_instance" "web" {
+    ami = "ami-0cc0a36f626a4fdf5"
+    count = 1
+    instance_type = "t2.micro"
+    key_name = "EP"
+    tags = {
+        Name = "ubuntu_1"
+        Group = "prod"
+
+    }
+  
+}
