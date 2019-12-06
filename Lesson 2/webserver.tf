4504ad56b91ca6f2d3d436546af2ca45ed0480a8
MastDon <sid2@yandex.ru>
2019-12-06 15:57:06 +0200

    Add config to .tf file project form Lesson2

--- Lesson 2/webserver.tf
+++ Lesson 2/webserver.tf
@@ -6,12 +6,24 @@ provider "aws" {
 resource "aws_instance" "web" {
     ami = "ami-0cc0a36f626a4fdf5"
     count = 1
+    
     instance_type = "t2.micro"
     key_name = "EP"
+    user_data = <<EOF
+#!/bin/bash
+apt update -y
+apt -y install apache2
+ip a > /var/www/index.html
+sudo systemctl start apache2
+sudo systemctl enable apache2
+EOF
     tags = {
-        Name = "ubuntu_1"
-        Group = "prod"
+        Name = "webserver"
+        Group = "test"
 
     }
   
 }
+
+
+
