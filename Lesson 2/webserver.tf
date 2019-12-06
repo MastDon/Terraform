85f19510b08cebac0a1377511b85af8704d97aa7
MastDon <sid2@yandex.ru>
2019-12-06 20:26:11 +0200

    Fix syntax bug

--- Lesson 2/webserver.tf
+++ Lesson 2/webserver.tf
@@ -18,7 +18,7 @@ ingress {
     to_port = 443
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-
+}
 egress {
 from_port = 0
 to_port = 0
@@ -26,7 +26,7 @@ protocol = "-1"
 cidr_blocks = ["0.0.0.0/0"]
 
 }
-}
+
 
 }
 
